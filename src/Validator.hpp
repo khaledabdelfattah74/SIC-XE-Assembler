f157c8b32b859522aeae24589255da334f14fc5f
khaledAbdelfattah74 <khaledabdelfattah74@gmail.com>
2018-04-29 16:25:17 +0200

    Adding Validator

---
+++ src/Validator.hpp
@@ -0,0 +1,27 @@
+//
+//  Validator.hpp
+//  SIC
+//
+//  Created by Khaled Abdelfattah on 4/29/18.
+//  Copyright © 2018 Khaled Abdelfattah. All rights reserved.
+//
+
+#ifndef Validator_hpp
+#define Validator_hpp
+
+#include <stdio.h>
+#include <string>
+
+#include "OpTable.hpp"
+
+using namespace std;
+
+class Validator {
+public:
+    Validator();
+    bool validate(string operation, string operand);
+private:
+    OpTable opTable;
+};
+
+#endif /* Validator_hpp */
