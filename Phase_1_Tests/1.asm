10ce561dd5d9723b0ce53386a72b9eb4ad9193d7
BassamMattar <37397559+BassamMattar@users.noreply.github.com>
2018-06-10 14:14:16 +0200

    Phase 1 test

---
+++ Phase_1_Tests/1.asm
@@ -0,0 +1,8 @@
+Prbn01   START   1000  
+         LDA     BETA  
+         MUL     GAMMA
+         STA     ALPHA
+ALPHA    RESW    1
+BETA     WORD    25
+GAMMA    WORD    4
+         END     Prbn01
