f3df144db14dc5ad7af166707087810730f1bf6f
BassamMattar <37397559+BassamMattar@users.noreply.github.com>
2018-06-11 10:51:43 +0200

    testing

--- src/Pass2.cpp
+++ src/Pass2.cpp
@@ -131,7 +131,9 @@ void Pass2::debugUtilities() {
 int Pass2::excute(string outPath) {
 
 	IntermediateFileParser intermediateParser = *new IntermediateFileParser(outPath);
+	cout << ".size()" << endl;
 	vector<IntermediateFileParser::entry> allEntryVector = intermediateParser.getEntriesVector();
+	cout << allEntryVector.size() << endl;
 	LabelProcessor labelProcessor = *new LabelProcessor();
 	unordered_map<string,string> labelAddresses = labelProcessor.assignLabelAddresses(&allEntryVector);
 	if(labelProcessor.getErrorFlag()) {
@@ -153,9 +155,9 @@ int Pass2::excute(string outPath) {
 	//debugLabelAddresses(labelAddresses);
 	//debugEntriesVectors(allEntryVector);
 	//debugAddressMode(allEntryVector);
-/*
+
 	ObjectProgramGenerator objGen = *new ObjectProgramGenerator();
-	objGen.generate_program_code(allEntryVector);*/
+	objGen.generate_program_code(allEntryVector);
 	return 1;
 }
 
